38e73198e4a431c3f331465b70500340afef768b
agustin <agustinpatat5@gmail.com>
2025-04-22 10:50:34 -0300

    Cuarto Commit

---
+++ shopping-list/src/components/ShoppingHeader.jsx
@@ -0,0 +1,9 @@
+// Componente simple que muestra el encabezado de la aplicación
+export function ShoppingHeader() {
+    return (
+      <div className="shopping-header">
+        <i className="fas fa-shopping-basket header-icon"></i>
+        <h1>Shopping List</h1>
+      </div>
+    )
+  }
