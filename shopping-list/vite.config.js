a07fc9b031f94469e9f3d6579d9aa99eec708b6a
agustin <agustinpatat5@gmail.com>
2025-05-21 08:02:56 -0300

    Actualizacion

--- shopping-list/vite.config.js
+++ shopping-list/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Shopping-List1/"
+  base: '/Shopping-List1/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
